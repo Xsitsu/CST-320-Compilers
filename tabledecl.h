80db6f53aed9f2f61e6d18451934bc7b561b6a8c
Xsitsu <jacoblocke21@live.com>
2021-01-14 14:38:30 -0800

    Updated and added other provided lab2 spec files.

---
+++ tabledecl.h
@@ -0,0 +1,37 @@
+//****************************************************
+// Starter code for a symbol table definition.
+// You are not required to use any of these names, but you will need the 
+// described functionality
+//
+class cSymbolTable
+{
+    public:
+        // Construct an empty symbol table
+        cSymbolTable();
+
+        // Increase the scope: add a level to the nested symbol table
+        // Return value is the newly created scope
+        symbolTable_t *IncreaseScope();
+
+        // Decrease the scope: remove the outer-most scope.
+        // Returned value is the outer-most scope AFTER the pop.
+        //
+        // NOTE: do NOT clean up memory after poping the table. Parts of the
+        // AST will probably contain pointers to symbols in the popped table.
+        symbolTable_t *DecreaseScope();
+
+        // insert a symbol into the table
+        // Assumes the symbol is not already in the table
+        void Insert(cSymbol *sym);
+
+        // Do a lookup in the nested table. 
+        // Return the symbol for the outer-most match. 
+        // Returns nullptr if no match is found.
+        cSymbol *Find(string name);
+
+        // Find a symbol in the outer-most scope.
+        // Returns nullptr if the symbol is not found.
+        cSymbol *FindLocal(string name);
+
+};
+
