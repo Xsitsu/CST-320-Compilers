14d87fe4550b56b90e85416da17e6f74b21b1172
Xsitsu <jacoblocke21@live.com>
2021-01-30 17:09:57 -0800

    Added cAstNode base class and updated cSymbol class with provided lab started code.

--- src/cSymbol.cpp
+++ src/cSymbol.cpp
@@ -9,22 +9,31 @@
 
 long long cSymbol::nextId = 0;
 
-cSymbol::cSymbol(std::string name)
+cSymbol::cSymbol(std::string name)  : cAstNode()
 {
     m_id = ++nextId;
     m_name = name;
 }
 
-std::string cSymbol::ToString()
-{
-    string result("<sym id=\"");
-    result += std::to_string(m_id);
-    result += "\" name=\"" + m_name + "\" />";
-
-    return result;
-}
-
 std::string cSymbol::GetName()
 {
     return m_name;
+}
+
+string cSymbol::AttributesToString()
+{
+    string result(" id=\"");
+    result += std::to_string(m_id);
+    result += "\" name=\"" + m_name + "\"";
+    return result;
+}
+
+string cSymbol::NodeType()
+{
+    return string("sym");
+}
+
+void cSymbol::Visit(cVisitor *visitor)
+{
+    visitor->Visit(this);
 }
