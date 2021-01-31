7385d549709f52394d57f4e8eb2818a637eba070
Xsitsu <jacoblocke21@live.com>
2021-01-30 16:56:12 -0800

    Created test_parse_tree routine.

---
+++ src/test_parse_tree.cpp
@@ -0,0 +1,56 @@
+//**************************************
+// test_parse_tree.cpp
+//
+// Test routine for the parse tree.
+//
+// Author: Jacob Locke
+//
+// Date: Jan. 30, 2021
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+//#include "cSymbolTable.h"
+#include "lex.h"
+#include "astnodes.h"
+#include "langparse.h"
+
+#include "fileopen.h"
+
+
+// **************************************************
+// argv[1] is the input file
+// argv[2] if present, is the output file
+int test_symbol_table(int argc, char **argv)
+{
+    int result = 0;
+    int open_result;
+
+    open_result = fileopen(argc, argv, yyin);
+    if (open_result != 0)
+    {
+        exit(open_result);
+    }   
+
+    result = yyparse();
+    if (yyast_root != nullptr)
+    {
+        if (result == 0)
+        {
+            output << yyast_root->ToString() << std::endl;
+        }
+        else
+        {
+            output << yynerrs << " Errors in compile\n";
+        }
+    }
+
+    if (result == 0 && yylex() != 0)
+    {
+        std::cout << "Junk at end of program\n";
+    }
+
+    return result;
+}
