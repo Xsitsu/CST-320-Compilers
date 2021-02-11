ed4282a53ff4b5b887d70b7359c2e41278d8590a
Xsitsu <jacoblocke21@live.com>
2021-02-10 23:21:11 -0800

    Removed some functions.

--- hdr/cBinaryExprNode.h
+++ hdr/cBinaryExprNode.h
@@ -20,21 +20,6 @@ class cBinaryExprNode : public cExprNode
             AddChild(new cOpNode(op));
             AddChild(right);
         }
-
-        cExprNode* GetLeft()
-        {
-            return static_cast<cExprNode*>(GetChild(0));
-        }
-
-        cExprNode* GetRight()
-        {
-            return static_cast<cExprNode*>(GetChild(2));
-        }
-        
-        cOpNode* GetOp()
-        {
-            return static_cast<cOpNode*>(GetChild(1));
-        }
         
         virtual string NodeType() { return string("expr"); }
         virtual void Visit(cVisitor *visitor) { visitor->Visit(this); }
