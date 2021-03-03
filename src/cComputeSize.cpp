05bb03973259efd0b1d61cd4055325b66865dc4b
Xsitsu <jacoblocke21@live.com>
2021-03-02 20:42:55 -0800

    Added functions for all other nodes in cComputeSize.

--- src/cComputeSize.cpp
+++ src/cComputeSize.cpp
@@ -7,6 +7,38 @@ void cComputeSize::VisitAllNodes(cAstNode *node)
     node->Visit(this);
 }
 
+
+void cVisitor::Visit(cAstNode *node)          { node->VisitAllChildren(this); }
+void cVisitor::Visit(cBinaryExprNode *node)   { node->VisitAllChildren(this); }
+void cVisitor::Visit(cIfNode *node)           { node->VisitAllChildren(this); }
+void cVisitor::Visit(cReturnNode *node)       { node->VisitAllChildren(this); }
+void cVisitor::Visit(cWhileNode *node)        { node->VisitAllChildren(this); }
+void cVisitor::Visit(cDeclNode *node)         { node->VisitAllChildren(this); }
+void cVisitor::Visit(cBaseTypeNode *node)     { node->VisitAllChildren(this); }
+void cVisitor::Visit(cArrayDeclNode *node)    { node->VisitAllChildren(this); }
+void cVisitor::Visit(cStructDeclNode *node)   { node->VisitAllChildren(this); }
+void cVisitor::Visit(cFuncDeclNode *node)     { node->VisitAllChildren(this); }
+void cVisitor::Visit(cParamsNode *node)       { node->VisitAllChildren(this); }
+void cVisitor::Visit(cVarExprNode *node)      { node->VisitAllChildren(this); }
+void cVisitor::Visit(cFuncExprNode *node)     { node->VisitAllChildren(this); }
+void cVisitor::Visit(cParamListNode *node)    { node->VisitAllChildren(this); }
+void cVisitor::Visit(cAssignNode *node)       { node->VisitAllChildren(this); }
+void cVisitor::Visit(cExprNode *node)         { node->VisitAllChildren(this); }
+void cVisitor::Visit(cIntExprNode *node)      { node->VisitAllChildren(this); }
+void cVisitor::Visit(cFloatExprNode *node)    { node->VisitAllChildren(this); }
+void cVisitor::Visit(cOpNode *node)           { node->VisitAllChildren(this); }
+void cVisitor::Visit(cPrintNode *node)        { node->VisitAllChildren(this); }
+void cVisitor::Visit(cStmtNode *node)         { node->VisitAllChildren(this); }
+void cVisitor::Visit(cStmtsNode *node)        { node->VisitAllChildren(this); }
+void cVisitor::Visit(cSymbol *node)           { node->VisitAllChildren(this); }
+
+
+
+
+
+
+
+
 void cComputeSize::Visit(cBlockNode *node)
 {
     node->VisitAllChildren(this);
